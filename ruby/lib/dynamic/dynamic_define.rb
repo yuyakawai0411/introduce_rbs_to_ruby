bae3570b723abb5ab63710aa342053d4843f9098
yuyakawai0411 <yuya.scuba0411@gmail.com>
2023-09-08 09:35:18 +0900

    typeの修正

--- ruby/lib/dynamic/dynamic_define.rb
+++ ruby/lib/dynamic/dynamic_define.rb
@@ -1,3 +1,16 @@
+# class Sample
+#   # @dynamic dynamic_method
+#   define_method :dynamic_method do
+#     "dynamic_method"
+#   end
+# end
+
+# def put_dynamic_method(sample)
+#   puts sample.dynamic_method
+# end
+
+# put_dynamic_method(Sample.new)
+
 class Sample
   # @dynamic dynamic_method
   define_method :dynamic_method do
@@ -5,9 +18,4 @@ class Sample
   end
 end
 
-def put_dynamic_method(sample)
-  puts sample.dynamic_method
-end
-
-put_dynamic_method(Sample.new)
-
+puts Sample.new.dynamic_method
